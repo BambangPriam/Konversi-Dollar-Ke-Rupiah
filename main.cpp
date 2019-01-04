1bb7868698cd1333be7553d28bc70d08f0ee45c8
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-04 22:26:47 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define nilairupiah  15193
+using namespace std;
+
+int main()
+
+{
+    float dollar;
+    cout << "Masukkan nilai dollar : ";
+    cin >> dollar;
+
+    cout << "Nilai rupiah adalah :" << "Rp. "<< nilairupiah*dollar;
+
+    return 0;
+}
